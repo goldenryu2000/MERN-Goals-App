97a405fb73dd734394c07071efceccb39b9ef19a
Nishant Sharma <goldenryu2000@gmail.com>
2022-05-20 15:56:36 +0530

    Added new Favicon and readme

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Goals App
+
+A Goals App made using MERN Stack.
+
+
+
+## Features
+
+- Multiple user support
+- Register New user
+- Create Goals (for each user)
+- Delete Goals (for each user)
+
+
+### Install dependencies
+
+```
+# Backend deps
+npm install
+# Frontend deps
+cd frontend
+npm install
+```
+    
+## Screenshots
+- Login Page
+![Login Page](https://i.imgur.com/ftsBfUk.png)
+- Register Page
+![Register Page](https://i.imgur.com/NykRIYc.png)
+- User Dashboard
+![Dashboard](https://i.imgur.com/eBEtBj8.png)
+
+
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FaSignInAlt, FaSignOutAlt, FaUser, FaGithub } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, reset } from "../features/auth/authSlice";
 function Header() {
@@ -15,17 +15,37 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">Goals App</Link>
+        <Link to="/">Goals App </Link>
       </div>
       <ul>
         {user ? (
-          <li>
-            <button className="btn" onClick={onLogout}>
-              <FaSignOutAlt /> LogOut
-            </button>
-          </li>
+          <>
+            <li>
+              <a
+                href="https://github.com/goldenryu2000/MERN-Goals-App"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub /> GitHub
+              </a>
+            </li>
+            <li>
+              <button className="btn" onClick={onLogout}>
+                <FaSignOutAlt /> LogOut
+              </button>
+            </li>
+          </>
         ) : (
           <>
+            <li>
+              <a
+                href="https://github.com/goldenryu2000/MERN-Goals-App"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub /> GitHub
+              </a>
+            </li>
             <li>
               <Link to="/login">
                 <FaSignInAlt /> Login
